e56a0fd3e768c1a50e0d719463c7235e8a3e4aac
Armisael2k <chuylopez755@gmail.com>
2022-12-08 14:28:17 -0700

    build

---
+++ build/precache-manifest.a1b097a8d8bdaf693d4a6f5ff74eeb2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e23d24e9a0003913506cf27831d040e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ec2ceb6bc391bddf55f",
+    "url": "/static/css/2.58337e42.chunk.css"
+  },
+  {
+    "revision": "1a8c3f9cca48f6142a75",
+    "url": "/static/css/main.441d783a.chunk.css"
+  },
+  {
+    "revision": "1ec2ceb6bc391bddf55f",
+    "url": "/static/js/2.33243107.chunk.js"
+  },
+  {
+    "revision": "1a8c3f9cca48f6142a75",
+    "url": "/static/js/main.f4298ebc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
